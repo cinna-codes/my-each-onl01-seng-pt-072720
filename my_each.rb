75181dd6cb67be09d9640a0a128d168ae9b0f5e6
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-05 03:28:25 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,10 @@
 def my_each(collection) # put argument(s) here
   # code here
   num = 0
+  arr = []
   while num < collection.length
-
+    arr << yield(collection[num])
+    i += 1
   end
+  arr
 end
